3e8d77bf33ef65904bd57e6c5e0a3b597945d33f
FUMOBOMBER <ah3082340@gmail.com>
2024-03-09 06:10:05 +0300

    addition: rk notification callback service done

---
+++ Construct.Rukassa/Implementation/RukassaPaymentNotificationCallbackService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Construct.Rukassa.Implementation;
+
+internal class RukassaPaymentNotificationCallbackService : IRukassaPaymentNotificationCallbackService
+{
+    private readonly ILogger<RukassaPaymentSuccessCallbackService> logger;
+
+    public RukassaPaymentNotificationCallbackService(
+        ILogger<RukassaPaymentSuccessCallbackService> logger)
+    {
+        this.logger = logger;
+    }
+
+    public RukassaPaymentNotificationCallbackRequest GetPaymentNotificationContent(HttpRequest request)
+    {
+        logger.LogDebug("{0}: payment notification dispatcher was triggered", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff"));
+        try
+        {
+            var requestObject = new RukassaPaymentNotificationCallbackRequest
+            {
+                Id = Convert.ToInt32(request.Form["id"]),
+                OrderId = Convert.ToInt32(request.Form["order_id"]),
+                Amount = Convert.ToDouble(request.Form["amount"]),
+                Way = Convert.ToString(request.Form["way"]),
+                Status = RukassaPaymentStatus.FromString(request.Form["status"]!),
+            };
+            return requestObject;
+        }
+        catch (ArgumentException ex)
+        {
+            logger.LogWarning(ex, "{0}: payment callback dispatcher issued with a warning ({1})", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff"), ex.Message);
+            throw;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "{0}: payment callback dispatcher issued with an error ({1})", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff"), ex.Message);
+            throw;
+        }
+    }
+}
---
+++ Construct.Rukassa/RukassaPaymentNotificationCallbackRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Construct.Rukassa;
+
+public class RukassaPaymentNotificationCallbackRequest
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public double Amount { get; set; }
+    public string Way { get; set; } = null!;
+    public RukassaPaymentStatus Status { get; set; } = null!;
+}
